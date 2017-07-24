ab68fbc632f87d18437138fd1e6cc9d09c04b633
Sidney Pham <sidneypham1@gmail.com>
2017-07-24 14:36:14 +1000

    Add SBHS OAuth2 modules. Just here to look nice for now.

---
+++ server/config_example.js
@@ -0,0 +1,9 @@
+// Fill this out and rename it to config.js.
+
+var config = {
+    clientId: '...',
+    clientSecret: '...',
+    host: '...' // e.g., 'localhost' (Don't add the port or protocol.)
+};
+
+module.exports.config = config;
