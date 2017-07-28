b8f34912a8c30a8c89cb22e364300748886e6d60
Sidney Pham <sidneypham1@gmail.com>
2017-07-29 01:01:12 +1000

    It's clean up my directory day.

--- server/User.js
+++ server/User.js
@@ -1,8 +1,4 @@
-const crypto = require('crypto');
 const db = require('./db');
-
-// const COLUMNS = ['username', 'password', 'role', 'created_at', 'updated_at'];
-// const TABLE = 'Members';
 
 class User {
   constructor(data) {
@@ -12,82 +8,11 @@ class User {
     this.phone = data.phone;
     this.username = data.username; // TODO: Remove?
     this.student_id = data.student_id;
-
     this.id = data.id;
   }
 
   get name() {
     return this.first_name + ' ' + this.surname;
-  }
-
-  getEmail() {
-    return this.email;
-  }
-
-  getPhone() {
-    return this.phone;
-  }
-
-  getUsername() {
-    return this.username;
-  }
-
-  getStudentId() {
-    return this.student_id;
-  }
-
-
-  updatePassword(newPassword, callback) {
-
-  }
-
-
-
-  static authenticate(username, password, callback) {
-    db.get('SELECT id, username, password FROM users WHERE username = ? LIMIT 1;',
-      [username], (err,row) => {
-      if(err) {
-        throw(err);
-      }
-
-      // convert to user object, and authenticate
-      if(row) {
-        // check if the password is correct
-        user = new User(row);
-        user.checkPassword(password, callback);
-      } else {
-        // return failed
-        callback(null, false);
-      }
-
-    });
-  }
-
-  // in place password check
-  checkPassword(password, callback) {
-    // perform some pbkdf2
-    var id = this.id,
-      hash = this.password.split('!'),
-      algorithm = hash[0],
-      iterations = parseInt(hash[1], 10),
-      salt = new Buffer(hash[2], 'base64'),
-      hash = new Buffer(hash[3], 'base64'),
-      hashSize = hash.length;
-
-    crypto.pbkdf2(password, salt, hashSize, iterations, algorithm,
-      (err, key) => {
-      if(err) return callback(null, false);
-      else callback(null, self.id);
-
-    });
-  }
-
-  orderedColumns(colList) {
-    var columns = Array(colList.length);
-    for(var i in colList) {
-      columns[i] = this[colList[i]];
-    }
-    return columns;
   }
 
   static getById(userId) {
@@ -97,27 +22,13 @@ class User {
     }).catch(err => {
       console.error(err);
       console.error(`Couldn\'t get user ${userId}.`);
-      // Populate a default user.
-      return new DefaultUser();
+      return null;
     });
   }
-}
 
-// TODO: Implement this.
-class DefaultUser extends User {
-  constructor() {
-    this.first_name = 'John';
-    this.surname = 'Citizen';
-    this.email = 'johncitizen@student.sbhs.nsw.edu.au';
-    this.phone = '123456789';
-    this.username = 'johncitizen'; // TODO: Remove?
-    this.student_id = '123';
-
-    this.id = null;
+  static getByStudentId(studentId) {
+    
   }
 }
 
-module.exports = {
-  User,
-  DefaultUser
-};
+module.exports = User;
